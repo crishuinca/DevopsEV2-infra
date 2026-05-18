f5edae6e515a51bb1c2d6ee2c9fc443da657b60e
crishuinca <cr.huinca@duouc.cl>
2026-05-18 15:53:09 -0400

    arreglo puerttos

--- etapa_2/main.tf
+++ etapa_2/main.tf
@@ -132,16 +132,16 @@ resource "aws_security_group" "backend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.backend_sg.id]
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    self      = true
   }
   ingress {
-    from_port       = 3307
-    to_port         = 3307
-    protocol        = "tcp"
-    security_groups = [aws_security_group.backend_sg.id]
+    from_port = 3307
+    to_port   = 3307
+    protocol  = "tcp"
+    self      = true
   }
   egress {
     from_port   = 0
